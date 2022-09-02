2258e45a25b0c0368bd57404b26f329a51098bfc
Oussama B <sevendevl7@gmail.com>
2022-09-02 18:13:01 +0200

    Validation & enum for Slot model
    Add enum :week_days as Hash
    
    Add validate which is checking that end_time must be
    After start_time
    
    Add rails-18n gem and config internalisation for Fr and en
    fr as default
    
    With its spec.

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,3 +1,23 @@
 class Slot < ApplicationRecord
   belongs_to :shop
+
+  validate :end_time_after_start_time
+
+  enum week_day: {
+    monday: 0,
+    tuesday: 1,
+    wednesday: 2,
+    thursday: 3,
+    friday: 4,
+    saturday: 5,
+    sunday: 6
+  }
+
+  private
+
+  def end_time_after_start_time
+    return unless end_time < start_time
+
+    errors.add(:end_time, :after_start_time)
+  end
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Slot, type: :model do
 
     it { expect(slot).to belong_to(:shop) }
   end
+
+  describe "validations" do
+    context "#end_time_after_start_time" do
+      let(:slot) { create(:slot, start_time: Time.now, end_time: Time.now - 60) }
+
+      it do
+        expect { slot }
+          .to raise_error(
+            ActiveRecord::RecordInvalid,
+            "La validation a échoué : End time #{I18n.t('activerecord.errors.models.slot.attributes.end_time.after_start_time')}"
+          )
+      end
+    end
+  end
 end
