3d32bcbda9d590029ac5f95a1701d7ae2a3c6f55
Oussama B <sevendevl7@gmail.com>
2022-09-02 18:18:16 +0200

    feat: Seeds
    Complete seed file to create:
    	- shop
    	- slots corresponding to the test demands

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+shop = Shop.create(name: "test")
+
+(0..6).each do |value|
+  next if value == 6
+
+  if value == 5
+    Slot.create!(
+      start_time: Time.new(2008, 6, 21, 11, 30),
+      end_time: Time.new(2008, 6, 21, 16, 00),
+      week_day: value.to_i,
+      shop: shop
+    )
+  else
+    Slot.create!(
+      start_time: Time.new(2008, 6, 21, 11, 30),
+      end_time: Time.new(2008, 6, 21, 16, 00),
+      week_day: value.to_i,
+      shop: shop
+    )
+    Slot.create!(
+      start_time: Time.new(2008, 6, 21, 18, 00),
+      end_time: Time.new(2008, 6, 21, 21, 00),
+      week_day: value.to_i,
+      shop: shop
+    )
+  end
+end
